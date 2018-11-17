e460c0f1d063d029cc77615c351ffeb699066166
Alvaro Perdiz <mac-alvaro@iMac-de-Alvaro.local>
2018-11-17 16:30:28 +0100

    Add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from 'common/filters/http-exception.filter';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Red Dead Redemption 2 | Animals API')
+    .setDescription('Red Dead Redemption 2 | Animals API')
+    .setVersion('1.0')
+    .setHost('localhost:3000')
+    .addTag('Animal')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
